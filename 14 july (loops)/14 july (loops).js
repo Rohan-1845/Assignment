0b6189cc38252108e8eaddbd544ec697ff9b81a0
Rohan-1845 <rohan7082253717@gmail.com>
2025-07-15 14:36:46 +0530

    14 july

---
+++ 14 july (loops)/14 july (loops).js
@@ -0,0 +1,67 @@
+/*
+let i = 0
+while(i<10)
+{
+console.log(i)
+i++
+}
+*/
+
+// 1. print 1-10 numbers
+let i 
+for(i=1; i<=10; i++)
+{
+    console.log(i)
+}
+
+// 2.print 10-1 numbers
+let j = 10
+while(j>=1)
+{
+console.log(j)
+j--
+}
+
+//  3.Print even numbers b/w 0-20
+
+let E
+for(e=0; e<=20; e+=2)
+{
+console.log(e)
+}
+
+
+// 4. print odd numbers b/w 1-20
+
+let odd 
+for(odd=1; odd<=30; odd+=2)
+{
+    console.log(odd)
+}
+
+// 5. print number which division by 5 b/w 1-50
+let div
+for(div=5; div<=50; div+=5)
+{
+    console.log(div)
+}
+
+
+// 6. print the sum of 1-100 number
+
+let sum =0
+for(i=1; i<=100; i++)
+    sum+=i
+{
+    console.log('Sum of 1-100 number is :', sum)
+}
+
+// 7. print the multiplication 1-10 number
+
+let mul =1
+for(i=1; i<=10; i++)
+    mul*=i
+{
+    console.log('Multiplication of 1-10 number is :', mul)
+}
+
